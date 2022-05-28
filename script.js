b8e447cb2c1cff0b1d4728cb977dabffb30fa579
Potayto <p.o.tayto@outlook.com>
2022-05-27 22:09:38 -0700

    Add content, styles, and behavior for displaying grid
    The grid displayed will be a 16x16 grid made up of div elements.

--- script.js
+++ script.js
@@ -1,39 +1,4 @@
 /*
-    > Create a webpage with a 16x16 grid of square divs.
-
-        index.html:
-
-        Add "container" div to HTML to hold the grid of divs;
-
-        *******************
-        script.js:
-
-        Get reference for "container" div and store in container variable;
-        for (count = 0; count < 16; count++)
-            Create div element;
-            Add class "row" to div;
-            Append div as child to container div;
-
-        Get reference for nodelist of "row" elements;
-        forEach of the elements in the nodelist:
-            for (count = 0; count < 16; count++)
-                Create a new div element;
-                Add class "grid-cell" to div;
-                Append div as child to each div with class "row";
-        
-        *******************
-        styles.css:
-
-        .container {
-            max-width: 2000px;
-        }
-
-        .row {
-            display: flex;
-            min-height: 3px;
-        }
-
-
     > Set up a “hover” effect so that the grid divs change color when your
     mouse passes over them, leaving a (pixelated) trail through your grid like
     a pen would.
@@ -56,3 +21,25 @@
     > Add a button to the top of the screen that will send the user a popup
     asking for the number of squares per side for the new grid.
 */
+
+
+const gridContainer = document.querySelector('#grid-container');
+
+for (count = 0; count < 16; count++)
+{
+    const div = document.createElement('div');
+    div.classList.toggle('row');
+    gridContainer.appendChild(div);
+}
+
+const rows = document.querySelectorAll('.row');
+
+rows.forEach((divElement) => {
+    for (count = 0; count < 16; count++)
+    {
+        const div = document.createElement('div');
+        div.classList.toggle('grid-cell');
+        div.textContent = count + 1;
+        divElement.appendChild(div);
+    }
+})
